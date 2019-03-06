46c07141b129b5fa2d12bc2c5ecd3c072935877a
Nick Anderegg <nick.anderegg@platform.sh>
2019-03-06 14:13:19 -0500

    Add twine command to config and increment to 2.0.4

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools.command.install import install
 
 cwd = os.path.abspath(os.path.dirname(__file__))
 
-VERSION = "2.0.3"
+VERSION = "2.0.4"
 
 with open('README.md', 'r', encoding='utf-8') as f:
     __readme__ = f.read()
