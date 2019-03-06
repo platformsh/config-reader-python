71e56dde154385635df8639e65a6719216756848
Nick Anderegg <nick.anderegg@platform.sh>
2019-03-06 13:14:29 -0500

    Update CircleCI config for deployment to PyPI

--- setup.py
+++ setup.py
@@ -10,14 +10,29 @@ from setuptools import setup, find_packages
 
 cwd = os.path.abspath(os.path.dirname(__file__))
 
+VERSION = "2.0.1"
+
 with open('README.md', 'r', encoding='utf-8') as f:
     __readme__ = f.read()
 
 with open('CHANGELOG.md', 'r', encoding='utf-8') as f:
     __changelog__ = f.read()
 
+class VerifyVersionCommand(install):
+    """Command to verify that the git tag matches our version"""
+    description = 'verify that the git tag matches our version'
+
+    def run(self):
+        tag = os.getenv('CIRCLE_TAG')
+
+        if tag != VERSION:
+            info = "Git tag: {0} does not match the version of this app: {1}".format(
+                tag, VERSION
+            )
+            sys.exit(info)
+
 setup(
-    version='2.0.0',
+    version=VERSION,
     name='platformshconfig',
     description='Small helper to access Platform.sh environment variables.',
     url='https://github.com/platformsh/platformsh-config-reader-python3',
@@ -37,4 +52,7 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3 :: Only'
     ],
+    cmdclass={
+        'verify': VerifyVersionCommand,
+    }
 )
