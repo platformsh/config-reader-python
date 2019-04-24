963ec98f38dd7ec8f9940bca00a92cbc7c223418
Larry Garfield <larry@platform.sh>
2019-04-24 16:57:53 -0500

    Bump version.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ from setuptools.command.install import install
 
 cwd = os.path.abspath(os.path.dirname(__file__))
 
-VERSION = "2.1.1"
+VERSION = "2.2.0"
 
 with open('README.md', 'r', encoding='utf-8') as f:
     __readme__ = f.read()
